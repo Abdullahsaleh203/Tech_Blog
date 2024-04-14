ac1b1764e4fbe0af9ed82f7e734d607865c73b72
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-04-14 19:23:54 +0200

    Fix email validation in RegistrationForm

--- tech_blog/forms.py
+++ tech_blog/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm 
 from wtforms import StringField ,PasswordField ,EmailField ,SubmitField ,BooleanField   
-from wtforms.validators import Length ,Email ,EqualTo, DataRequired 
-from email_validator import validate_email, EmailNotValidError
-
+from wtforms.validators import Length ,Email ,EqualTo, DataRequired  ,ValidationError
+from email_validator import validate_email, EmailNotValidError 
+from tech_blog.models import User 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                             validators=[DataRequired(),Length(min=6,max=25)])
@@ -16,7 +16,10 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Conform password',
                                 validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Sign Up')
-    
+    def validate_felid(self, field):
+        
+        if True:
+            raise ValidationError('Invalid email address.')
 class LoginForm(FlaskForm):
 
     email = EmailField('email', 
