185fabf47b6cc3d3f6f72de61614d34eb502f04f
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-05-09 22:14:10 +0300

    Update main.c to improve code organization and remove unnecessary comments

--- tech_blog/__init__.py
+++ tech_blog/__init__.py
@@ -10,6 +10,6 @@ app.config['SECRET_KEY'] = '6f2ce3abf636b7a22328e3e1162a90cf'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager()
+login_manager = LoginManager(app)
 # login_manager.init_app(app)
 from tech_blog import routes
