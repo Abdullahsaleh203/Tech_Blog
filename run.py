4aa1991b4b3cf3db48a76180e0689d97f379b9b5
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-05-09 23:41:35 +0300

    Refactor run.py to update the port number for debugging

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy 
 from tech_blog import app
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=5000)
 db = SQLAlchemy(app)
 from tech_blog import routes
